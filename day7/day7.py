3c492879e2b7d5e029d15a9d5d683ef6f889ab45
Olli Helminen <opadele@gmail.com>
2020-11-12 22:00:15 +0200

    Added solution for day 7 part 2

--- day7/day7.py
+++ day7/day7.py
@@ -1,4 +1,4 @@
-
+"""https://adventofcode.com/2016/day/7"""
 import re
 
 
@@ -11,12 +11,34 @@ def abba_finder(string):
     return False
 
 
+def supports_ssl(supernets, hypernets):
+    """Find out of IP supports SSL"""
+    abas = []
+    for sequence in supernets:
+        for idx in range(len(sequence)-2):
+            if sequence[idx] == sequence[idx+2]:
+                if sequence[idx] != sequence[idx+1]:
+                    abas.append(sequence[idx:idx+3])
+    if not abas:
+        return False
+    for aba in abas:
+        bab = aba[1] + aba[0] + aba[1]
+        for hyper in hypernets:
+            if bab in hyper:
+                return True
+    return False
+
+
 count = 0
+ssl_count = 0
 with open('./day7/input.txt') as f:
     data = f.readlines()
 for ip in data:
     strings = re.split(r'[\[\]]', ip.strip('\n'))
     # every odd string is inside brackets
+    supernets = strings[::2]
+    hypernets = strings[1::2]
+    ssl_count += supports_ssl(supernets, hypernets)
     results = []
     for idx, string in enumerate(strings):
         if not idx % 2:
@@ -27,4 +49,5 @@ for ip in data:
     else:
         count += any(results)
 
-print(count)
+print(f'Supports TLS: {count}')
+print(f'Supports SSL: {ssl_count}')
